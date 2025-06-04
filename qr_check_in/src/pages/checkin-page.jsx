cf40ec91fb05135c566c2a7d464c4b1f7d0c8637
Vincent Chen <vince@Vincents-Mac-mini.local>
2025-06-04 02:42:37 -0700

    handleCheckIn implemented

--- qr_check_in/src/pages/checkin-page.jsx
+++ qr_check_in/src/pages/checkin-page.jsx
@@ -28,7 +28,7 @@ const CheckInPage = () => {
 
   const updateCheckInList = async () => {
     const data = await Utils.getSheetData(accessToken, spreadsheetId, checkInListSheetTitle, `R1C1:R1048576C6`, "ROWS", navigate);
-
+    
     var tempColObj = {};
     for (let index = 0; index < data[0].length; index++) {
       tempColObj[data[0][index]] = index ;
@@ -40,7 +40,7 @@ const CheckInPage = () => {
     var tempCheckInData = {}
     for (let rowIndex = 1; rowIndex < dataLen; rowIndex++){
       var row = data[rowIndex];
-      var rowData = {};
+      var rowData = {"rowIndex": rowIndex + 1};
       var rowEmail = "";
       for (const colTitle in tempColObj){
         let colIndex = tempColObj[colTitle];
@@ -49,28 +49,76 @@ const CheckInPage = () => {
         }
         if (colTitle === "Email"){
           rowEmail = row[colIndex];
-        }else{
-          rowData[colTitle] = row[colIndex];
-          if(colTitle === "Check-In" && row[tempColObj[colTitle]] != undefined && row[tempColObj[colTitle]] != ""){
-            checkedInCount++;
-          }
+        }
+        rowData[colTitle] = row[colIndex];
+        if(colTitle === "Check-In" && row[tempColObj[colTitle]] != undefined && row[tempColObj[colTitle]] != ""){
+          checkedInCount++;
         }
       }
       tempCheckInData[rowEmail] = rowData;
     }
-
-    console.log(tempCheckInData)
-
+    
     setTotalRegistrations(dataLen - 1);
     setCheckedIn(checkedInCount);
     setNotCheckedIn(dataLen - checkedInCount - 1);
-
+    setCheckInData(tempCheckInData);
+    
     console.log("Check In List Updated!");
     setTimeout(updateCheckInList, 3000);
   }
+  
+  const alreadyCheckedIn = (email) => {
+    if (!(email in checkInData))return false;
+    const data = checkInData[email];
+    if ("Check-In" in data && data["Check-In"] != undefined && data["Check-In"] != "")return true;
+    return false;
+  }
 
-  const handleCheckIn = async () => {
+  const modifyCheckInData = (email, values) => {
+    var data = checkInData;
+    for (const colTitle in values){
+      data[email][colTitle] = values[colTitle];
+    }
+    console.log(data);
+    setCheckInData(data);
+    return data;
+  }
 
+  const generateUpdateRow = (email) => {
+    var tempRow = [];
+    const rowData = checkInData[email];
+    for (const colTitle in checkInListColumnsObj){
+      const colIndex = checkInListColumnsObj[colTitle];
+      while (tempRow.length - 1 < colIndex) {tempRow.push("")}
+      tempRow[colIndex] = rowData[colTitle];
+    }
+    return tempRow;
+  }
+
+  const handleCheckIn = async (email) => {
+    console.log(checkInData);
+    if (!(email in checkInData)){
+      console.log(`Error: ${email} is not in the check-in list`);
+      return false;
+    }
+    const curTime = Utils.getTime();
+    if (alreadyCheckedIn(email)){
+      console.log(`Warning: ${email} has already checked in`)
+      
+      const rowData = modifyCheckInData(email, {"Last Seen": curTime})[email];
+      const updateRowData = generateUpdateRow(email);
+      Utils.updateSheetData(accessToken, spreadsheetId, checkInListSheetTitle,
+                            `R${rowData["rowIndex"]}C1:R${rowData["rowIndex"]}C${updateRowData.length}`,
+                            "ROWS", [updateRowData], navigate);
+                            return false;
+    }
+    const rowData = modifyCheckInData(email, {"Check-In": curTime, "Last Seen": curTime})[email];
+    const updateRowData = generateUpdateRow(email);
+    console.log(updateRowData);
+    Utils.updateSheetData(accessToken, spreadsheetId, checkInListSheetTitle,
+                          `R${rowData["rowIndex"]}C1:R${rowData["rowIndex"]}C${updateRowData.length}`,
+                          "ROWS", [updateRowData], navigate);
+    return true;
   }
 
   useEffect(() => {
@@ -105,6 +153,7 @@ const CheckInPage = () => {
     <div className="pageContainer">
       <div className={styles.contentBox}>
         <h4>{spreadsheetName === "" ? "Loading..." : spreadsheetName}</h4>
+        <button onClick={() => {handleCheckIn("vincent.tw99367@gmail.com")}}></button>
         <h5>Total Registrations: {totalRegistrations} / Checked In: {checkedIn} / Not Checked In: {notCheckedIn}</h5>
         <div id="reader"></div>
       </div>
